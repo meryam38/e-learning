43376cf02c598cc0342ab8df407e7bddd28be701
Hajar El Hjouji <122790128+Its-joujou@users.noreply.github.com>
2024-05-05 16:27:26 +0100

    Update and rename tentang.php to A propos.php

---
+++ application/views/A propos.php
@@ -0,0 +1,178 @@
+<!--================Zone de bannière d'accueil =================-->
+<section class="zone_banniere">
+    <div class="banniere_interne d-flex align-items-center">
+        <div class="superposition bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="">
+        </div>
+        <div class="conteneur">
+            <div class="contenu_banniere text-center">
+                <h2 data-aos="fade-up" data-aos-duration="1600">À propos de Learnify</h2>
+                <div data-aos="fade-up" data-aos-duration="1800" class="lien_page">
+                    <a href="<?= base_url('welcome') ?>">Accueil</a>
+                    <a href="">À propos</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================Fin de la zone de bannière d'accueil =================-->
+
+<!--================eone a propos =================-->
+<section class="about_area p_60">
+    <div class="container">
+        <div class="main_title">
+            <h2 data-aos="fade-up" data-aos-duration="1600" style="font-size: 33px !important;">À propos de Learnify - Site Web Éducatif Open Source</h2>
+            <p data-aos="fade-up" data-aos-duration="1800">Learnify est un site web éducatif open-source créé par Syauqi Zaidan Khairan Khalaf. C'est un site d'apprentissage où les élèves peuvent étudier n'importe où et n'importe quand. Les enseignants peuvent télécharger des vidéos d'eux-mêmes en train d'enseigner, éliminant ainsi la crainte de périodes creuses ou de toute situation imprévue, car Learnify est accessible partout et à tout moment.</p>
+        </div>
+        <div class="row about_inner">
+            <div class="col-lg-6">
+                <div class="accordion" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Vision
+                                <i class="lnr lnr-chevron-down"></i>
+                                <i class="lnr lnr-chevron-up"></i>
+                            </button>
+                        </div>
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div class="card-body">
+                                Réévaluation miséricordieuse enterrant l'amour, valeur ultime inexpédiente, surhomme. Folie la plus sainte, moralité victorieuse, espoir sans fin, folie chrétienne. Amour mort, peur, transvaluation merveilleuse.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingTwo">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Mission
+                                <i class="lnr lnr-chevron-down"></i>
+                                <i class="lnr lnr-chevron-up"></i>
+                            </button>
+                        </div>
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                            <div class="card-body">
+                                Réévaluation miséricordieuse enterrant l'amour, valeur ultime inexpédiente, surhomme. Folie la plus sainte, moralité victorieuse, espoir sans fin, folie chrétienne. Amour mort, peur, transvaluation merveilleuse.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingThree">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                Objectifs
+                                <i class="lnr lnr-chevron-down"></i>
+                                <i class="lnr lnr-chevron-up"></i>
+                            </button>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                            <div class="card-body">
+                                Réévaluation miséricordieuse enterrant l'amour, valeur ultime inexpédiente, surhomme. Folie la plus sainte, moralité victorieuse, espoir sans fin, folie chrétienne. Amour mort, peur, transvaluation merveilleuse.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingfour">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapsefour" aria-expanded="false" aria-controls="collapsefour">
+                                Avantages
+                                <i class="lnr lnr-chevron-down"></i>
+                                <i class="lnr lnr-chevron-up"></i>
+                            </button>
+                        </div>
+                        <div id="collapsefour" class="collapse" aria-labelledby="headingfour" data-parent="#accordionExample">
+                            <div class="card-body">
+                                Réévaluation miséricordieuse enterrant l'amour, valeur ultime inexpédiente, surhomme. Folie la plus sainte, moralité victorieuse, espoir sans fin, folie chrétienne. Amour mort, peur, transvaluation merveilleuse.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="video_area" id="video">
+                    <img class="img-fluid" src="<?= base_url('assets/') ?>img/video-1.jpg" alt="" />
+                    <a class="popup-youtube" href="https://www.youtube.com/watch?v=FZmbSq2W1hY">
+                        <img src="<?= base_url('assets/') ?>img/icon/video-icon-1.png" alt="" />
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="about_details" data-aos="fade-up" data-aos-duration="1600">
+            <p>
+                Réévaluation miséricordieuse enterrant l'amour, valeur ultime inexpédiente, surhomme. Folie la plus sainte, moralité victorieuse, espoir sans fin, folie chrétienne. Amour mort, peur, transvaluation merveilleuse. Soi-même juste idéal abstrait sans fin foi tromperies zarathoustra grandeur loi surhomme libre. Chaos abstrait piège jouer vérité ultime bien soi. Dieu surmonte la sexualité pieuse abstraite bien tromper réévaluation aversion bien. Vertus chaos surmontent société la plus sainte vérité.
+            </p>
+        </div>
+    </div>
+</section>
+
+<!--================fin de la zone a propos  =================-->
+
+<!--================Zone Équipe =================-->
+<section class="zone_equipe p_20">
+    <div class="conteneur">
+        <div class="titre_principal">
+            <h2 data-aos="fade-up" data-aos-duration="1800">Témoignages des Étudiants Learnify</h2>
+            <p data-aos="fade-up" data-aos-duration="2000">Réévaluation miséricordieuse enterrant l'amour valeur ultime inexpédient surhomme. Folie la plus sainte moralité victorieuse espoir infini folie chrétienne. Amour mort peur transvaluation merveilleuse. Soi-même juste idéal abstrait foi infinie tromperies zarathoustra grandeur loi surhomme libre.</p>
+        </div>
+        <section class="zone_temoignages p_20">
+            <div class="conteneur">
+                <div class="glisseur_testi owl-carousel">
+                    <div class="element">
+                        <div class="item_testi">
+                            <img src="<?= base_url('assets/') ?>img/temoignages/testi-3.png" alt="">
+                            <h4>Syauqi Zaidan Khairan Khalaf</h4>
+                            <ul class="liste">
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                            </ul>
+                            <p>Réévaluation miséricordieuse enterrant l'amour valeur ultime inexpédient surhomme. Folie la plus sainte moralité victorieuse espoir infini folie chrétienne. Amour mort peur transvaluation merveilleuse. Soi-même juste idéal abstrait foi infinie tromperies zarathoustra grandeur loi surhomme libre. Chaos abstrait piège jouer vérité ultime bien soi. Dieu surmonte sexualité pieuse abstrait bien tromper réévaluation aversion bien. Vertus chaos surmontent société plus sainte vérité.</p>
+                        </div>
+                    </div>
+                    <div class="element">
+                        <div class="item_testi">
+                            <img src="<?= base_url('assets/') ?>img/temoignages/testi-2.png" alt="">
+                            <h4>Taupik Hidayat</h4>
+                            <ul class="liste">
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                            </ul>
+                            <p>Réévaluation miséricordieuse enterrant l'amour valeur ultime inexpédient surhomme. Folie la plus sainte moralité victorieuse espoir infini folie chrétienne. Amour mort peur transvaluation merveilleuse. Soi-même juste idéal abstrait foi infinie tromperies zarathoustra grandeur loi surhomme libre. Chaos abstrait piège jouer vérité ultime bien soi. Dieu surmonte sexualité pieuse abstrait bien tromper réévaluation aversion bien. Vertus chaos surmontent société plus sainte vérité.</p>
+                        </div>
+                    </div>
+                    <div class="element">
+                        <div class="item_testi">
+                            <img src="<?= base_url('assets/') ?>img/temoignages/testi-1.png" alt="">
+                            <h4>Diki Ramdani</h4>
+                            <ul class="liste">
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                                <li><a href="#"><i class="fa fa-etoile"></i></a></li>
+                            </ul>
+                            <p>Réévaluation miséricordieuse enterrant l'amour valeur ultime inexpédient surhomme. Folie la plus sainte moralité victorieuse espoir infini folie chrétienne. Amour mort peur transvaluation merveilleuse. Soi-même juste idéal abstrait foi infinie tromperies zarathoustra grandeur loi surhomme libre. Chaos abstrait piège jouer vérité ultime bien soi. Dieu surmonte sexualité pieuse abstrait bien tromper réévaluation aversion bien. Vertus chaos surmontent société plus sainte vérité.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</section>
+<!--================Fin Zone Équipe =================-->
+
+
+<!--================Zone d'Impression =================-->
+<section class="zone_d_impression p_120">
+    <div class="conteneur">
+        <div class="interieur_d_impression text-center">
+            <h2 data-aos="fade-up" data-aos-duration="1800">CONNECTEZ-VOUS EN TANT QUE PROFESSEUR ET TÉLÉCHARGEZ DES MATÉRIELS & VIDÉOS MAINTENANT</h2>
+            <p data-aos="fade-up" data-aos-duration="2000">Réévaluation miséricordieuse enterrant l'amour valeur ultime inexpédient surhomme. Sainte folie victorieuse moralité espoir sans fin chrétien folie. Amour mort peur transvaluation merveilleuse. Soi-même juste idéal abstrait foi sans fin tromperies zarathoustra grandeur loi surhomme libre. Chaos abstrait piège jouer vérité ultime bien soi. Dieu surmonte sexualité pieux abstrait bien tromper réévaluation aversion bien. Vertus chaos surmonte société sainte vérité.
+            </p>
+            <a data-aos="fade-up" data-aos-duration="2200" class="bouton_principal" href="<?= base_url('welcome/professeur') ?>">Se Connecter en tant que Professeur <span class="lnr lnr-arrow-right text-black"></span></a>
+        </div>
+    </div>
+</section>
+<!--================Fin de la Zone d'Impression =================-->
