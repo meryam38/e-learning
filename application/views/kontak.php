45f6199e8191341ba444036b32dbe684037b21d7
Hajar El Hjouji <122790128+Its-joujou@users.noreply.github.com>
2024-05-05 15:55:15 +0100

    Update kontak.php

--- application/views/kontak.php
+++ application/views/kontak.php
@@ -1,41 +1,42 @@
-<!--================Home Banner Area =================-->
-<section class="banner_area">
-    <div class="banner_inner d-flex align-items-center">
-        <div class="kontak bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="">
+<!--================Zone de la Bannière d'Accueil =================-->
+<section class="zone_de_la_banniere">
+    <div class="interieur_de_la_banniere d-flex align-items-center">
+        <div class="contact bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="">
         </div>
-        <div class="container">
-            <div class="banner_content text-center">
-                <h2 data-aos="fade-up" data-aos-duration="1600">Kontak</h2>
-                <div data-aos="fade-up" data-aos-duration="1600" class="page_link">
-                    <a href="<?= base_url('welcome') ?>">Beranda</a>
-                    <a href="<?= base_url('welcome/kontak') ?>">Kontak</a>
+        <div class="conteneur">
+            <div class="contenu_de_la_banniere text-center">
+                <h2 data-aos="fade-up" data-aos-duration="1600">Contact</h2>
+                <div data-aos="fade-up" data-aos-duration="1600" class="lien_page">
+                    <a href="<?= base_url('welcome') ?>">Accueil</a>
+                    <a href="<?= base_url('welcome/contact') ?>">Contact</a>
                 </div>
             </div>
         </div>
     </div>
 </section>
-<!--================End Home Banner Area =================-->
+<!--================Fin de la Zone de la Bannière d'Accueil =================-->
 
-<!--================Contact Area =================-->
-<section class="contact_area p_40">
-    <div class="container">
-        <div style="width: 100%"><iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=SMKN%201%20CIAMIS+(Learnify)&amp;ie=UTF8&amp;t=&amp;z=18&amp;iwloc=A&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">gps coordinates</a></iframe></div><br />
-        <div class="row">
+
+<!--================Zone de Contact =================-->
+<section class="zone_de_contact p_40">
+    <div class="conteneur">
+        <div style="width: 100%"><iframe width="100%" height="600" src="https://maps.google.com/maps?width=100%&height=600&hl=fr&q=SMKN%201%20CIAMIS+(Learnify)&ie=UTF8&t=&z=18&iwloc=A&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">coordonnées gps</a></iframe></div><br />
+        <div class="ligne">
             <div class="col-lg-9">
-                <div class="contact_info">
-                    <div class="info_item">
+                <div class="informations_de_contact">
+                    <div class="element_info">
                         <i class="lnr lnr-home"></i>
                         <h6>Jl.Jendral Soedirman No.269
                             Ciamis, Jawa Barat</h6>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                     </div>
-                    <div class="info_item">
+                    <div class="element_info">
                         <i class="lnr lnr-phone-handset"></i>
                         <h6><a href="#">
                                 (0265) 771204</a></h6>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                     </div>
-                    <div class="info_item">
+                    <div class="element_info">
                         <i class="lnr lnr-envelope"></i>
                         <h6><a href="#">apps.learnify@gmail.com</a></h6>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
@@ -45,4 +46,4 @@
         </div>
     </div>
 </section>
-<!--================Contact Area =================-->
+<!--================Fin de la Zone de Contact =================-->
