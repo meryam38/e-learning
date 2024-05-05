d537d0b96fb98158d74ba5bd94360358931785ee
aminovic99 <aminekh890@gmail.com>
2024-05-05 16:08:21 +0100

    modif index

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -17,7 +17,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-    <title>Admin Dashboard - Learnify</title>
+    <title>Tableau de bord d'administration - Learnify</title>
     <!-- General CSS Files -->
     <link rel="icon" href="<?= base_url('assets/') ?>img/favicon.png" type="image/png">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700;900&display=swap" rel="stylesheet">
@@ -44,7 +44,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                 <ul class="navbar-nav navbar-right">
                     <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
                             <img alt="image" style="margin-bottom:4px !important;" src="./assets/stisla-assets/img/avatar/avatar-2.png" class="rounded-circle mr-1 my-auto border-white">
-                            <div class="d-sm-none d-lg-inline-block" style="font-size:15px;">Hello, <?php
+                            <div class="d-sm-none d-lg-inline-block" style="font-size:15px;">Bonjour, <?php
                                                                                                     $data['user'] = $this->db->get_where('admin', ['email' =>
                                                                                                     $this->session->userdata('email')])->row_array();
                                                                                                     echo $data['user']['username'];
@@ -53,7 +53,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                         <div class="dropdown-menu dropdown-menu-right">
                             <div class="dropdown-title">Admin - Learnify</div>
                             <a href="<?= base_url('welcome/logout') ?>" class="dropdown-item has-icon text-danger">
-                                <i class="fas fa-sign-out-alt"></i> Logout
+                                <i class="fas fa-sign-out-alt"></i> Se déconnecter
                             </a>
                         </div>
                     </li>
@@ -71,49 +71,50 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                         <a href="<?= base_url('admin') ?>">LY <sup>3</sup></a>
                     </div>
                     <ul class="sidebar-menu">
-                        <li class="menu-header ">Dashboard</li>
+                        <li class="menu-header ">Tableaux de bord</li>
                         <li class="nav-item dropdown active">
-                            <a href="<?= base_url('admin') ?>" class="nav-link"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
+                            <a href="<?= base_url('admin') ?>" class="nav-link"><i class="fas fa-desktop"></i><span>Tableaux de bord</span></a>
                         </li>
-                        <li class="menu-header">Management Siswa</li>
-                        <li class="nav-item dropdown">
+                        <li class="menu-header">Gestion des étudiants</li>
+                        <li class="nav-item dropdown ">
                             <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
-                                <span>Siswa</span></a>
+                                <span>Étudiant</span></a>
                             <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="<?= base_url('admin/data_siswa') ?>">Data Siswa</a></li>
+                                <li><a class="nav-link" href="<?= base_url('admin/data_siswa') ?>">Données sur les étudiants</a></li>
                             </ul>
                         </li>
-                        <li class="menu-header">Management Guru</li>
-                        <li class="nav-item dropdown">
+                        <li class="menu-header">gestion des Professeurs </li>
+                        <li class="nav-item dropdown active">
                             <a href="#" class="nav-link has-dropdown"><i class="fas fa-chalkboard-teacher"></i>
-                                <span>Guru</span></a>
+                                <span>Professeur</span></a>
                             <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="<?= base_url('admin/data_guru') ?>">Data Guru</a>
+                                <li><a class="nav-link" href="<?= base_url('admin/data_guru') ?>">Données sur les enseignants</a>
                                 </li>
-                                <li><a class="nav-link" href="<?= base_url('admin/add_guru') ?>">Tambah Data Guru</a>
+                                <li><a class="nav-link" href="<?= base_url('admin/add_guru') ?>">Ajouter des données sur l'enseignant</a>
                                 </li>
                             </ul>
                         </li>
-                        <li class="menu-header">Management Materi</li>
+                        <li class="menu-header">La gestion du matériel</li>
                         <li class="nav-item dropdown">
                             <a href="#" class="nav-link has-dropdown"><i class="fas fa-book"></i>
-                                <span>Materi</span></a>
+                                <span>Matériel</span></a>
                             <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="<?= base_url('admin/data_materi') ?>">Data Materi</a>
+                                <li><a class="nav-link" href="<?= base_url('admin/data_materi') ?>">Données matérielles</a>
                                 </li>
-                                <li><a class="nav-link" href="<?= base_url('admin/tambah_materi') ?>">Tambah Materi</a>
+                                <li><a class="nav-link" href="<?= base_url('admin/tambah_materi') ?>">Ajouter du matériel</a>
                                 </li>
+
                             </ul>
                         </li>
-                        <li class="menu-header">About Developer</li>
+                        <li class="menu-header">À propos du développeur</li>
                         <li class="nav-item dropdown">
                             <a href="#" class="nav-link has-dropdown"><i class="fas fa-address-card"></i>
-                                <span>Developer</span></a>
+                                <span>développeur</span></a>
                             <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="<?= base_url('admin/about_developer') ?>">Tentang
-                                        Pembuat</a>
+                                <li><a class="nav-link" href="<?= base_url('admin/about_developer') ?>">À propos
+                                        Fabricant</a>
                                 </li>
-                                <li><a class="nav-link" href="<?= base_url('admin/about_learnify') ?>">Tentang
+                                <li><a class="nav-link" href="<?= base_url('admin/about_learnify') ?>">À propos
                                         Learnify</a>
                                 </li>
                             </ul>
@@ -126,7 +127,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
             <div class="main-content">
                 <section class="section">
                     <div class="section-header">
-                        <h1 style="font-size: 27px; letter-spacing:-0.5px; color:black;">Dashboard</h1>
+                        <h1 style="font-size: 27px; letter-spacing:-0.5px; color:black;">Tableaux de bord</h1>
                     </div>
                     <div class="row">
                         <div class="col-lg-3 col-md-6 col-sm-6 col-12">
@@ -136,7 +137,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                                 </div>
                                 <div class="card-wrap">
                                     <div class="card-header">
-                                        <h4>Siswa</h4>
+                                        <h4>Étudiant</h4>
                                     </div>
                                     <div class="card-body">
                                         <?php echo $this->db->count_all('siswa'); ?>
@@ -151,7 +152,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                                 </div>
                                 <div class="card-wrap">
                                     <div class="card-header">
-                                        <h4>Guru</h4>
+                                        <h4>Professeur</h4>
                                     </div>
                                     <div class="card-body">
                                         <?php echo $this->db->count_all('guru'); ?>
@@ -166,7 +167,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                                 </div>
                                 <div class="card-wrap">
                                     <div class="card-header">
-                                        <h4>Materi</h4>
+                                        <h4>Matériel</h4>
                                     </div>
                                     <div class="card-body">
                                         <?php echo $this->db->count_all('materi'); ?>
@@ -181,7 +182,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                                 </div>
                                 <div class="card-wrap">
                                     <div class="card-header">
-                                        <h4>Kelas</h4>
+                                        <h4>Classe</h4>
                                     </div>
                                     <div class="card-body">
                                         3
@@ -193,25 +194,25 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                     <div class="">
                         <div class="card" style="width:100%;">
                             <div class="card-body">
-                                <h2 class="card-title" style="color: black;">Tambah Materi?</h2>
+                                <h2 class="card-title" style="color: black;">Ajouter du matériel ?</h2>
                                 <hr>
-                                <p class="card-text">After I ran into Helen at a restaurant, I realized she was just office pretty drop-dead date put in in a deck for our standup today. Who's responsible for the ask for this request? who's responsible for the ask for this request? but moving the goalposts gain traction.</p>
-                                <a href="<?= base_url('admin/tambah_materi') ?>" class="btn btn-success">Tambah Materi ⭢</a>
+                                <p class="card-text">Après avoir croisé Helen dans un restaurant, j'ai réalisé qu'elle n'était qu'un rendez-vous au bureau mis dans une présentation pour notre stand-up d'aujourd'hui. Qui est responsable de cette demande ? qui est responsable de cette demande ? mais déplacer les poteaux de but gagne du terrain.</p>
+                                <a href="<?= base_url('admin/tambah_materi') ?>" class="btn btn-success">Ajouter du matériel ⭢</a>
                             </div>
                         </div>
                     </div>
                     <div class="">
                         <div class="hero text-white hero-bg-image" data-background="<?= base_url('assets/') ?>stisla-assets/img/unsplash/eberhard-grossgasteiger-1207565-unsplash.jpg">
                             <div class=" hero-inner">
-                                <h1>Selamat Datang, <?php
+                                <h1>ACCUEILLIR, <?php
                                                     $data['user'] = $this->db->get_where('admin', ['email' =>
                                                     $this->session->userdata('email')])->row_array();
                                                     echo $data['user']['username'];
                                                     ?>!</h1>
-                                <p class="lead">After I ran into Helen at a restaurant, I realized she was just office pretty drop-dead date put in in a deck for our standup today. Who's responsible for the ask for this request? but moving the goalposts gain traction.</p>
+                                <p class="lead">Après avoir croisé Helen dans un restaurant, j'ai réalisé qu'elle n'était qu'un rendez-vous au bureau mis dans une présentation pour notre stand-up d'aujourd'hui. Qui est responsable de cette demande ? mais déplacer les poteaux de but gagne du terrain.</p>
                                 <div class="mt-4">
                                     <a href="<?= base_url('admin/data_siswa') ?>" class="btn btn-outline-white btn-lg btn-icon icon-left"><i class="far fa-user"></i>
-                                        Data Siswa</a>
+                                    Données sur les étudiants</a>
                                 </div>
                             </div>
                         </div>
