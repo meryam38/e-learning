106a188043d320790b5917f54add5a53bb05cb51
Hajar El Hjouji <122790128+Its-joujou@users.noreply.github.com>
2024-05-05 16:03:04 +0100

    Update and rename pelajaran.php to lecons.php

---
+++ application/views/lecons.php
@@ -0,0 +1,116 @@
+<!--================Zone de la Bannière d'Accueil =================-->
+<section class="zone_de_la_banniere">
+    <div class="interieur_de_la_banniere d-flex align-items-center">
+        <div class="lecons bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background="">
+        </div>
+        <div class="conteneur">
+            <div class="contenu_de_la_banniere text-center">
+                <h2 data-aos="fade-up" data-aos-duration="1600">Leçons</h2>
+                <div data-aos="fade-up" data-aos-duration="1800" class="lien_page">
+                    <a href="<?= base_url('welcome') ?>">Accueil</a>
+                    <a href="<?= base_url('welcome/lecons') ?>">Leçons</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================Fin de la Zone de la Bannière d'Accueil =================-->
+
+<!--================Zone des Cours =================-->
+<section class="zone_des_cours p_40">
+    <div class="conteneur">
+        <div class="titre_principal">
+            <h2 data-aos="fade-up" data-aos-duration="1600">Cours Disponibles sur Learnify</h2>
+            <p data-aos="fade-up" data-aos-duration="1800">Réévaluation miséricordieuse enterrant l'amour valeur ultime inexpédient surhomme. Sainte folie victorieuse moralité espoir sans fin chrétien folie. Amour mort peur transvaluation merveilleuse. Soi-même juste idéal abstrait foi sans fin tromperies zarathoustra grandeur loi surhomme libre.</p>
+        </div>
+        <div class="ligne cours_interne">
+            <div class="col-lg-9">
+                <div class="interieur_grille">
+                    <div class="element_grille wd55">
+                        <div class="element_cours" data-aos="fade-right" data-aos-duration="1800">
+                            <img src="<?= base_url('assets/') ?>img/cours/cours-1.jpg" alt="">
+                            <div class="texte_survol">
+                                <a class="categorie" href="#">Gratuit</a>
+                                <a href="javaScript:void(0);">
+                                    <h4>Cours de Mathématiques Gratuit</h4>
+                                </a>
+                                <ul class="liste">
+                                    <li><a href="#"><i class="lnr lnr-users"></i>54</a></li>
+                                    <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+                                    <li><a href="#"><i class="lnr lnr-user"></i>Professeur de Mathématiques SMKN 1 Ciamis</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="element_grille wd44">
+                        <div class="element_cours" data-aos="fade-down" data-aos-duration="1800">
+                            <img src="<?= base_url('assets/') ?>img/cours/cours-2.jpg" alt="">
+                            <div class="texte_survol">
+                                <a class="categorie" href="#">Gratuit</a>
+                                <a href="javaScript:void(0);">
+                                    <h4>Cours de Sciences Gratuit</h4>
+                                </a>
+                                <ul class="liste">
+                                    <li><a href="#"><i class="lnr lnr-users"></i> 34</a></li>
+                                    <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+                                    <li><a href="#"><i class="lnr lnr-user"></i>Professeur de Sciences SMKN 1 Ciamis</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="element_grille wd44">
+                        <div class="element_cours" data-aos="fade-right" data-aos-duration="1800">
+                            <img src="<?= base_url('assets/') ?>img/cours/cours-4.jpg" alt="">
+                            <div class="texte_survol">
+                                <a class="categorie" href="#">Gratuit</a>
+                                <a href="javaScript:void(0);">
+                                    <h4>Cours d'Anglais Gratuit</h4>
+                                </a>
+                                <ul class="liste">
+                                    <li><a href="#"><i class="lnr lnr-users"></i> 63</a></li>
+                                    <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+                                    <li><a href="#"><i class="lnr lnr-user"></i>Professeur d'Anglais SMKN 1 Ciamis</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="element_grille wd55">
+                        <div class="element_cours" data-aos="fade-up" data-aos-duration="1800">
+                            <img src="<?= base_url('assets/') ?>img/cours/cours-5.jpg" alt="">
+                            <div class="texte_survol">
+                                <a class="categorie" href="#">Gratuit</a>
+                                <a href="javaScript:void(0);">
+                                    <h4>Cours de Bahasa Indonésien Gratuit</h4>
+                                </a>
+                                <ul class="liste">
+                                    <li><a href="#"><i class="lnr lnr-users"></i> 24</a></li>
+                                    <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+                                    <li><a href="#"><i class="lnr lnr-user"></i>Professeur de Bahasa Indonésien SMKN 1 Ciamis</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="element_cours" data-aos="fade-left" data-aos-duration="1800">
+                    <img src="<?= base_url('assets/') ?>img/cours/cours-3.jpg" alt="">
+                    <div class="texte_survol">
+                        <a class="categorie" href="#">Gratuit</a>
+                        <a href="javaScript:void(0);">
+                            <h4>Cours d'Éducation Religieuse Islamique Gratuit</h4>
+                        </a>
+                        <ul class="liste">
+                            <li><a href="#"><i class="lnr lnr-users"></i> 35</a></li>
+                            <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
+                            <li><a href="#"><i class="lnr lnr-user"></i>Professeur de Religion SMKN 1 Ciamis</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--================Fin de la Zone des Cours =================-->
