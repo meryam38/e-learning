0424b17ca2633e4515c2d41480651917176ebd86
aminovic99 <aminekh890@gmail.com>
2024-05-05 14:52:46 +0100

    modification about_developer

--- application/views/admin/about_developer.php
+++ application/views/admin/about_developer.php
@@ -17,7 +17,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
 <head>
 	<meta charset="UTF-8">
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-	<title>Tentang Developer - Learnify</title>
+	<title>À propos du développeur - Learnify</title>
 	<!-- General CSS Files -->
 	<link rel="icon" href="<?= base_url('assets/') ?>img/favicon.png" type="image/png">
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700;900&display=swap" rel="stylesheet">
@@ -54,7 +54,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
 						<div class="dropdown-menu dropdown-menu-right">
 							<div class="dropdown-title">Admin - Learnify</div>
 							<a href="<?= base_url('welcome/logout') ?>" class="dropdown-item has-icon text-danger">
-								<i class="fas fa-sign-out-alt"></i> Logout
+								<i class="fas fa-sign-out-alt"></i> Se déconnecter
 							</a>
 						</div>
 					</li>
@@ -72,52 +72,54 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
 						<a href="<?= base_url('admin') ?>">LY</a>
 					</div>
 					<ul class="sidebar-menu">
-						<li class="menu-header ">Dashboard</li>
+						<li class="menu-header ">Tableaux de bord
+
+</li>
 						<li class="nav-item dropdown ">
-							<a href="<?= base_url('admin') ?>" class="nav-link"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
+							<a href="<?= base_url('admin') ?>" class="nav-link"><i class="fas fa-desktop"></i><span>Tableaux de bord</span></a>
 						</li>
-						<li class="menu-header">Management Siswa</li>
+						<li class="menu-header">Gestion des étudiants</li>
 						<li class="nav-item dropdown">
 							<a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
-								<span>Siswa</span></a>
+								<span>Étudiant</span></a>
 							<ul class="dropdown-menu">
-								<li><a class="nav-link" href="<?= base_url('admin/data_siswa') ?>">Data Siswa</a></li>
+								<li><a class="nav-link" href="<?= base_url('admin/data_siswa') ?>">Données sur les étudiants</a></li>
 							</ul>
 						</li>
-						<li class="menu-header">Management Guru</li>
+						<li class="menu-header">Professeur de gestion</li>
 						<li class="nav-item dropdown">
 							<a href="#" class="nav-link has-dropdown"><i class="fas fa-chalkboard-teacher"></i>
-								<span>Guru</span></a>
+								<span>Professeur</span></a>
 							<ul class="dropdown-menu">
-								<li><a class="nav-link" href="<?= base_url('admin/data_guru') ?>">Data Guru</a>
+								<li><a class="nav-link" href="<?= base_url('admin/data_guru') ?>">Données sur les enseignants</a>
 								</li>
-								<li><a class="nav-link" href="<?= base_url('admin/add_guru') ?>">Tambah Data Guru</a>
+								<li><a class="nav-link" href="<?= base_url('admin/add_guru') ?>">Ajouter des données sur l'enseignant</a>
 								</li>
 
 							</ul>
 						</li>
-						<li class="menu-header">Management Materi</li>
+						<li class="menu-header">La gestion du matériel</li>
 						<li class="nav-item dropdown">
 							<a href="#" class="nav-link has-dropdown"><i class="fas fa-book"></i>
-								<span>Materi</span></a>
+								<span>Matériel</span></a>
 							<ul class="dropdown-menu">
-								<li><a class="nav-link" href="<?= base_url('admin/data_materi') ?>">Data Materi</a>
+								<li><a class="nav-link" href="<?= base_url('admin/data_materi') ?>">Données matérielles</a>
 								</li>
-								<li><a class="nav-link" href="<?= base_url('admin/tambah_materi') ?>">Tambah Materi</a>
+								<li><a class="nav-link" href="<?= base_url('admin/tambah_materi') ?>">Ajouter du matériel</a>
 								</li>
 
 							</ul>
 						</li>
-						<li class="menu-header">About Developer</li>
+						<li class="menu-header">À propos du développeur</li>
 						<li class="nav-item dropdown active">
 							<a href="#" class="nav-link has-dropdown"><i class="fas fa-address-card"></i>
-								<span>Developer</span></a>
+								<span>développeur</span></a>
 							<ul class="dropdown-menu">
-								<li><a class="nav-link" href="<?= base_url('admin/about_developer') ?>">Tentang
-										Pembuat</a>
+								<li><a class="nav-link" href="<?= base_url('admin/about_developer') ?>">À propos
+Fabricant</a>
 								</li>
-								<li><a class="nav-link" href="<?= base_url('admin/about_learnify') ?>">Tentang
-										Learnify</a>
+								<li><a class="nav-link" href="<?= base_url('admin/about_learnify') ?>">À propos
+Apprendre</a>
 								</li>
 							</ul>
 						</li>
@@ -141,7 +143,7 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
 							<div class="card-body">
 								<h2 class="card-title" style="color: black;">Syauqi Zaidan Khairan Khalaf</h2>
 								<hr>
-								<p class="card-text">Perfectionist Web Developer with one years of experience as a Web Developer and Web Designer. Skilled at Designing and developing Websites. Excellent written and oral communication skills; capable of explaining complex software issues in easy-to-understand terms. </p>
+								<p class="card-text">Développeur Web perfectionniste avec un an d'expérience en tant que développeur Web et concepteur Web. Compétent dans la conception et le développement de sites Web. Excellentes compétences en communication écrite et orale ; capable d'expliquer des problèmes logiciels complexes en termes faciles à comprendre.</p>
 							</div>
 						</div>
 					</div>
